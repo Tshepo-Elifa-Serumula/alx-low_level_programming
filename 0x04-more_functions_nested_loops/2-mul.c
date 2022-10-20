8dc509b40371be1b4012376fad03359cf22fc2d4
Tshepo-Elifa-Serumula <tshepoelifa238@gmail.com>
2022-10-20 02:22:07 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers together
+ * @a: first number
+ * @b: second number
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
