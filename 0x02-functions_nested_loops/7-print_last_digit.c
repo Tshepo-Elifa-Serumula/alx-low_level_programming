180c866415eaefe041aab570ee6be45d68386b12
Tshepo-Elifa-Serumula <tshepoelifa238@gmail.com>
2022-10-19 23:05:27 -0700

    New

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,22 +4,14 @@
 
 /**
  *
- *  *  print_last_digit - last digit
+ * print_last_digit - last digit
  *
- *   *
  *
- *    *@n: The int to print
+ * @n: The int to print
  *
- *     * Return: Always 0.
- *
- *      */
-
-
-
+ * Return: Always 0.
+ */
 int print_last_digit(int n)
-
-
-
 {
 
 	int last_digit;
